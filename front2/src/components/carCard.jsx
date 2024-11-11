3026b8a4548ebe2510d1e27c7723c291379b8799
lorencete-2003 <l.ramirezcampo@gmail.com>
2024-11-11 18:57:49 +0100

    styles and img of carCard

--- front2/src/components/carCard.jsx
+++ front2/src/components/carCard.jsx
@@ -1,52 +1,44 @@
 // CarCard.jsx
 import React from 'react';
 import { Link } from 'react-router-dom';
-import '../assets/css/styleguide.css';
 import '../assets/css/style.css';
+import gas from '../assets/img/gas-station.svg';
+import transmission from '../assets/img/Car.svg';
+import cap from '../assets/img/profile-2user.svg';
 
 function CarCard({ vehicle }) {
   return (
     <div className="catalog">
       <div className="car-name">
         <div className="text-wrapper">{vehicle.make} {vehicle.model}</div>
-        <div className="div">Sport</div>
+        <img className="like" src="/assets/images/like.svg" alt="Like" />
       </div>
-      <img className="like" src="/assets/images/like.svg" alt="Like" />
-      <div className="price">
-        <div className="text-wrapper-2">{vehicle.price} €</div>
-      </div>
-      <Link to={`/vehicles/${vehicle._id}`} className="button-rental">
-        <div className="text-wrapper-3">Rent</div>
-      </Link>
+      <img className="car-2" src={vehicle.image} alt={`${vehicle.make} ${vehicle.model}`} />
+      
       <div className="spesification">
-        <div className="div-2">
-          <img className="img" src="/assets/images/gas-station.svg" alt="Fuel Type" />
-          <div className="text-wrapper-4">{vehicle.fuelCapacity}L</div>
+        <div>
+          <img className="img" src={gas} alt="Fuel Type" />
+          <span>{vehicle.fuelCapacity}L</span>
         </div>
-        <div className="car">
-          <div className="img">
-            <div className="overlap-group">
-              <div className="circle"></div>
-              <img className="circle-2" src="/assets/images/circle-2.svg" alt="Circle" />
-              <div className="circle-3"></div>
-              <div className="rectangle"></div>
-              <div className="rectangle-2"></div>
-              <div className="rectangle-3"></div>
-            </div>
-          </div>
-          <div className="text-wrapper-5">{vehicle.transmission}</div>
+        <div>
+          <img className="img" src={transmission} alt="Transmission" />
+          <span>{vehicle.transmission}</span>
         </div>
-        <div className="div-2">
-          <img className="img" src="/assets/images/profile-2user.svg" alt="Capacity" />
-          <div className="text-wrapper-6">{vehicle.capacity} People</div>
+        <div>
+          <img className="img" src={cap} alt="Capacity" />
+          <span>{vehicle.capacity} People</span>
         </div>
       </div>
-      <div className="overlap">
-        <div className="shadow"></div>
-        <img className="car-2" src={vehicle.image} alt={`${vehicle.make} ${vehicle.model}`} />
+      
+      <div className="price-and-button">
+        <div className="price">{vehicle.price} €</div>
+        <Link to={`/vehicles/${vehicle._id}`}>
+          <button className="button-rental">Rent</button>
+        </Link>
       </div>
     </div>
   );
 }
 
 export default CarCard;
+
